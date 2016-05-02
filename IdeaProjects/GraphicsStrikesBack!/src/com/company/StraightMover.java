622d68420a4a8880e924d2559d60af97e061e89e
Miku Imai <mirai.331423@gmail.com>
2016-05-02 18:44:19 +0900

    Option

--- IdeaProjects/GraphicsStrikesBack!/src/com/company/StraightMover.java
+++ IdeaProjects/GraphicsStrikesBack!/src/com/company/StraightMover.java
@@ -1,35 +1,21 @@
 package com.company;
 
-import java.awt.*;
-
 import java.awt.Graphics;
 
 public class StraightMover {
-    private int x;
-    private int y;
-    private int xDirection;
-    private int yDirection;
-    private Sprite sprite;
+    private MoveSetting setting;
 
-    /** Create a Bouncer that positions sprite at (startX, startY). */
-    public StraightMover(int startX, int startY, Sprite sprite) {
-        x = startX;
-        y = startY;
-        this.sprite = sprite;
-    }
 
-    /** Starts moving the object in the direction (xIncrement, yIncrement). */
-    public void setMovementVector(int xIncrement, int yIncrement) {
-        xDirection = xIncrement;
-        yDirection = yIncrement;
+    public StraightMover(MoveSetting set) {
+        setting = set;
     }
 
     public void draw(Graphics graphics) {
-        sprite.draw(graphics, x, y);
+        setting.mover.draw(graphics, setting.x, setting.y);
 
         // Move the center of the object each time we draw it
-        x += xDirection;
-        y += yDirection;
+        setting.x += setting.xDirection;
+        setting.y += setting.yDirection;
     }
 }
 
