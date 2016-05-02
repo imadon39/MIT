2dfd93f84c71fe7c5824b0b97d005d9bf9794248
Miku Imai <mirai.331423@gmail.com>
2016-05-02 18:42:25 +0900

    Option

--- IdeaProjects/GraphicsStrikesBack!/src/com/company/Bouncer.java
+++ IdeaProjects/GraphicsStrikesBack!/src/com/company/Bouncer.java
@@ -3,45 +3,32 @@ package com.company;
 import java.awt.Graphics;
 
 public class Bouncer {
-    private int x;
-    private int y;
-    private int xDirection;
-    private int yDirection;
-    private Sprite sprite;
+    private MoveSetting setting;
 
-    /** Create a Bouncer that positions sprite at (startX, startY). */
-    public Bouncer(int startX, int startY, Sprite sprite, int xIncrement, int yIncrement) {
-        x = startX;
-        y = startY;
-        this.sprite = sprite;
-        setMovementVector(xIncrement,yIncrement);
-    }
 
-    /** Starts moving the object in the direction (xIncrement, yIncrement). */
-    public void setMovementVector(int xIncrement, int yIncrement) {
-        xDirection = xIncrement;
-        yDirection = yIncrement;
+    public Bouncer(MoveSetting set) {
+        setting = set;
     }
 
     /** Draws the sprite at its current position on to surface. */
     public void draw(Graphics surface) {
         // Draw the sprite
-        sprite.draw(surface, x, y);
+        setting.mover.draw(surface, setting.x, setting.y);
 
         // Move the object each time we draw it
-        x += xDirection;
-        y += yDirection;
+        setting.x += setting.xDirection;
+        setting.y += setting.yDirection;
 
         // If we have hit the edge and are moving in the wrong direction, reverse direction
         // We check the direction because if a box is placed near the wall, we would get "stuck"
         // rather than moving in the right direction
-        if ((x <= 0 && xDirection < 0) ||
-                (x + sprite.getWidth() >= SimpleDraw.WIDTH && xDirection > 0)) {
-            xDirection = -xDirection;
+        if ((setting.x <= 0 && setting.xDirection < 0) ||
+                (setting.x + setting.mover.getWidth() >= SimpleDraw.WIDTH && setting.xDirection > 0)) {
+            setting.xDirection = -setting.xDirection;
         }
-        if ((y <= 0 && yDirection < 0) ||
-                (y + sprite.getHeight() >= SimpleDraw.HEIGHT && yDirection > 0)) {
-            yDirection = -yDirection;
+        if ((setting.y <= 0 && setting.yDirection < 0) ||
+                (setting.y + setting.mover.getHeight() >= SimpleDraw.HEIGHT && setting.yDirection > 0)) {
+            setting.yDirection = -setting.yDirection;
         }
     }
 }
