deb17dd1600389873a9f3a1dcc98214c076dd8c6
Miku Imai <mirai.331423@gmail.com>
2016-05-02 09:44:24 +0900

    Part2

--- IdeaProjects/MIT/src/com/company/GravityCalculator.java
+++ IdeaProjects/MIT/src/com/company/GravityCalculator.java
@@ -10,7 +10,11 @@ public class GravityCalculator {
         double fallingTime = 10.0;
         double initialPosition = 0.0;
         double finalPosition = 0.0;
+
+        finalPosition = 0.5 * gravity * Math.pow(fallingTime,2) + initialVelocity * fallingTime + initialPosition;
+
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
     }
+    //Part1では代入した定数のみを表示
 }
